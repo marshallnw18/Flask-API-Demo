b07cf17a60268b2e7084dac760ae68998364aa39
Nick Marshall <marshallnw18@gmail.com>
2020-10-01 22:11:37 -0400

    added additional api functionality for demo

---
+++ README.md
@@ -0,0 +1,7 @@
+1. Cover how to initialize the SQLite database 
+2. Cover the purpose of the application and how it demonstrates a RESTful API
+3. Benefits of Flask?
+4. Requirements and "requirements.txt" installation with pip 
+5. Explanation of test.py and how to run it along with the Flask app 
+
+Finish from 'Updating Data' section of the Tech With Tim Video
--- test.py
+++ test.py
@@ -2,5 +2,15 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld")
+data = [{"likes": 41082, "name": "Day at the Zoo", "views": 200431},
+        {"likes": 24908, "name": "Hiking the AT", "views": 100951},
+        {"likes": 20091, "name": "Flask Web App Development", "views": 192780}
+]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
